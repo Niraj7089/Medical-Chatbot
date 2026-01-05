a2113fae012b150b4bbe00170eeedabf547baa4c
Niraj7089 <nirajdhiwar.pune@gmail.com>
2026-01-04 23:03:04 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Niraj Dhiwar",
+    author_email="nirajdhiwar.pune@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
